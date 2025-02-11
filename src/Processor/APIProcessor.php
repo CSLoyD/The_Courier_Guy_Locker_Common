86f47d2402d655a131e9698eb6ac291ca94d094b
web2.loyd <chrisnilavila@gmail.com>
2025-02-11 22:15:34 +0800

    Initial

---
+++ src/Processor/APIProcessor.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace The_Courier_Guy_Locker\Common\Processor;
+
+class APIProcessor
+{
+    private string $apiUrlBase;
+
+    public const API_METHODS = [
+        'get_all_lockers' => [
+            'type'     => 'GET',
+            'endpoint' => '/api/v1/lockers-data',
+        ],
+        'get_rates'       => [
+            'type'     => 'POST',
+            'endpoint' => '/api/v1/rates'
+        ],
+        'booking_request' => [
+            'type'     => 'POST',
+            'endpoint' => '/api/v1/shipments'
+        ],
+        'get_waybill'     => [
+            'type'     => 'GET',
+            'endpoint' => '/generate/waybill'
+        ],
+        'locker_rates'    => [
+            'type'     => 'GET',
+            'endpoint' => '/api/v1/locker-rates'
+        ],
+    ];
+
+    /**
+     * @param string $apiUrlBase
+     */
+    public function __construct(string $apiUrlBase)
+    {
+        $this->apiUrlBase = $apiUrlBase;
+    }
+
+    /**
+     * @param $getLockersResponse
+     *
+     * @return array
+     */
+    public static function mapLockers($getLockersResponse): array
+    {
+        $getLockersResponse = json_decode($getLockersResponse, true);
+        $mappedLockers      = [];
+        array_map(function ($locker) use (&$mappedLockers) {
+            $mappedLockers[$locker['code']] = $locker;
+        }, $getLockersResponse);
+
+        return $mappedLockers;
+    }
+
+    /**
+     * @param $method
+     * @param $content
+     *
+     * @return array
+     */
+    public function getRequest($method, $content): array
+    {
+        $url  = $this->apiUrlBase . self::API_METHODS[$method]['endpoint'];
+        $type = self::API_METHODS[$method]['type'];
+
+        if ($type === 'GET' && $content) {
+            $url .= $content;
+        }
+
+        return ['url' => $url, 'type' => $type];
+    }
+}
